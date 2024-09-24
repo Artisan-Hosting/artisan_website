7b4a352fd5e6bc07425922743ab1dc50029062c4
Darrion Whitfield <dwhitfield@ramfield.net>
2024-09-24 18:40:20 -0500

    FIX: Scroll to top on new pages

--- src/pages/Process.js
+++ src/pages/Process.js
@@ -2,9 +2,14 @@ import HostingProcess from '../components/Process';
 import NavBar from '../components/Navbar/NavBar';
 import Footer from '../components/Footer';
 import { useDocTitle } from '../components/CustomHook';
+import { useEffect } from 'react';
 // impoty useDocTitle
 
 const Process = () => {
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
+      }, []);
+
     useDocTitle('The Artisan Process');
 
     return (
--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -2,13 +2,20 @@ import React from 'react';
 import NavBar from '../components/Navbar/NavBar';
 import Footer from '../components/Footer';
 import { useDocTitle } from '../components/CustomHook';
+import { useEffect } from "react";
 import Cal from '@calcom/embed-react';
 
 
 const Calendar = () => {
     useDocTitle('Artisan Hosting: Schedule initial consultation');
+    
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
+      }, []);
+
     return (
         <>
+        
             <div>
                 <NavBar />
             </div>
