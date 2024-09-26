9e33e97f35fed59abc726c0e23bb45e17e1bd07a
Darrion Whitfield <dwhitfield@ramfield.net>
2024-09-26 17:51:53 -0500

    ADD: More information to the Process page

--- src/components/Process.js
+++ src/components/Process.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import imgConsultation from '../images/consultation.svg';
 import imgHosting from '../images/hosting.svg';
 import imgWebDev from '../images/web-dev.svg';
 import imgDeveloper from '../images/Web-developer.svg';
-import imgComputer from '../images/web.svg'
+import imgComputer from '../images/web.svg';
+
+// Modal Component
+const Modal = ({ show, onClose, title, content }) => {
+    if (!show) return null;
+    
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+            <div className="bg-white rounded-lg shadow-lg p-6 w-11/12 max-w-md">
+                <h3 className="text-2xl font-bold text-blue-900 mb-4">{title}</h3>
+                <p className="text-gray-700 mb-6">{content}</p>
+                <button onClick={onClose} className="text-white bg-blue-900 hover:bg-blue-800 px-4 py-2 rounded-md">
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+};
 
 const HostingProcess = () => {
+    const [showModal, setShowModal] = useState(true);
+    const [modalContent, setModalContent] = useState({ title: 'An error has occurred', content: 'This has been open at the wrong time. Please use the contact button in the footer to report this error' });
+
+    const handleOpenModal = (title, content) => {
+        setModalContent({ title, content });
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <div className="bg-gray-100 py-12 lg:mt-4">
             <section data-aos="fade-up">
-                {/* Introduction */}
                 <div className="my-4 py-4">
                     <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our Hosting Process</h2>
                     <div className='flex justify-center'>
@@ -21,11 +49,14 @@ const HostingProcess = () => {
                     </h2>
                 </div>
 
-                {/* Steps */}
                 <div className="px-12" data-aos="fade-down" data-aos-delay="600">
                     <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-8">
                         {/* Step 1: Consultation */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => 
+                                handleOpenModal('Step 1: Consultation', 'Schedule a meeting with our experts to discuss your unique goals and pain points with your application and its hosting. This will include things like, Compliance, Uptime requirements, Technologies used, Designated downtimes. This will ensure that we are all on the same page before we begin our work and research')}
+                        >
                             <img alt="Consultation" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgConsultation} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 1: Consultation</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -34,7 +65,11 @@ const HostingProcess = () => {
                         </div>
 
                         {/* Step 2: Requirement Analysis */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => 
+                                handleOpenModal('Step 2: Requirement Analysis', 'We analyze your requirement to create a customized hosting plan. This will outline building processes and ideal deployment specifications for your application.')}
+                        >
                             <img alt="Requirement Analysis" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgWebDev} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 2: Requirement Analysis</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -43,7 +78,11 @@ const HostingProcess = () => {
                         </div>
 
                         {/* Step 3: Custom Solution Design */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => 
+                                handleOpenModal('Step 3: Custom Solution Design', 'Our team executes what we\'ve defined in the hosting plan. This may even included building systems on our end to ensure reliability and scalability for your needs.')}
+                        >
                             <img alt="Custom Solution Design" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgComputer} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 3: Custom Solution Design</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -52,7 +91,10 @@ const HostingProcess = () => {
                         </div>
 
                         {/* Step 4: Implementation */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => handleOpenModal('Step 4: Implementation', 'We handle the entire implementation process, from migrations to setup, ensuring everything runs smoothly.')}
+                        >
                             <img alt="Implementation" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgHosting} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 4: Implementation</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -61,7 +103,11 @@ const HostingProcess = () => {
                         </div>
 
                         {/* Step 5: Launch */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => 
+                                handleOpenModal('Step 5: Launch', 'We assist you through the launch process, making sure your site is live without any issues.')}
+                        >
                             <img alt="Launch" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgWebDev} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 5: Launch</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -70,7 +116,10 @@ const HostingProcess = () => {
                         </div>
 
                         {/* Step 6: Ongoing Support and Monitoring */}
-                        <div className="bg-white rounded-lg shadow-2xl p-6 group">
+                        <div
+                            className="bg-white rounded-lg shadow-2xl p-6 group cursor-pointer"
+                            onClick={() => handleOpenModal('Step 6: Ongoing Support', 'We provide continuous monitoring and support to ensure your hosting environment stays optimized.')}
+                        >
                             <img alt="Support and Monitoring" className="h-24 mx-auto group-hover:scale-110 transition duration-500" src={imgDeveloper} />
                             <h3 className="font-semibold my-4 text-2xl text-center text-blue-900">Step 6: Ongoing Support</h3>
                             <p className="text-md font-medium text-gray-700 text-center">
@@ -79,6 +128,14 @@ const HostingProcess = () => {
                         </div>
                     </div>
                 </div>
+
+                {/* Modal for More Information */}
+                <Modal
+                    show={showModal}
+                    onClose={handleCloseModal}
+                    title={modalContent.title}
+                    content={modalContent.content}
+                />
 
                 {/* Call to Action */}
                 <div className="text-center mt-12">
